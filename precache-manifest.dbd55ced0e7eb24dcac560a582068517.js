d6cf5cdda9465c33bc3107558d09af4d73ec4812
harshinir007 <harshu07.r@gmail.com>
2023-05-05 17:52:04 +0530

    Updates

---
+++ precache-manifest.dbd55ced0e7eb24dcac560a582068517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e504726fe6850bda9371",
+    "url": "/lms-aassign/static/css/main.16f8d0fa.chunk.css"
+  },
+  {
+    "revision": "e504726fe6850bda9371",
+    "url": "/lms-aassign/static/js/main.e504726f.chunk.js"
+  },
+  {
+    "revision": "6b7477f59c074214b942",
+    "url": "/lms-aassign/static/js/1.6b7477f5.chunk.js"
+  },
+  {
+    "revision": "510d6a1ddedfaf151ffe",
+    "url": "/lms-aassign/static/js/runtime~main.510d6a1d.js"
+  },
+  {
+    "revision": "2f1af3e4ecae622fa20a34843e608cb4",
+    "url": "/lms-aassign/index.html"
+  }
+];
